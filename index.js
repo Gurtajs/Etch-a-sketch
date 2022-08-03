e9baac0632dadd5c865896dc3e52193b37b12f77
Gurtaj <gurtajs98@gmail.com>
2022-08-03 16:10:53 +0100

    finished

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
 const container = document.querySelector('#container');
-
 const div = document.createElement('div');
-div.classList.add('small');
-container.appendChild(div);
 
 
+const getRandomNumber = (maxNum) => {
+     return Math.floor(Math.random()*maxNum)
+ };
 
+const getRandomColour = () => {
+     const h = getRandomNumber(360);
+     const s = getRandomNumber(100);
+     const l = getRandomNumber(100);
 
-function grid(rows, columns) {
-    
-    container.setAttribute('style', `grid-template-rows: repeat(${rows}, 18px); grid-template-columns: repeat(${columns}, 18px)`);
-    for (c = 1; c < rows*columns; c++) {
-    const div = document.createElement('div');
-    div.classList.add('small');
-    container.appendChild(div);
-    }
+     return `hsl(${h}deg, ${s}%, ${l}%)`;
+ };
+
+// const ranCol = () => {
+//     div.addEventListener('mouseover', () => {
+//         div.style.backgroundColor = getRandomColour();
+//         })
+// }
+
+const button = document.querySelector('.button')
+const button2 = document.querySelector('.button2')
+
+ function grid(rows, columns) {
+    for (let c = 0; c < rows*columns; c++) {
+        const div = document.createElement('div');
+        div.classList.add('small');
+        container.style.gridTemplateColumns = `repeat(${rows}, 1fr)`
+        container.style.gridTemplateRows = `repeat(${columns}, 1fr)`
+        container.appendChild(div);
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = getRandomColour();
+    })}
+}
+grid(16, 16);
+
+function reset() {
+    const divs = container.querySelectorAll('.small')
+    divs.forEach(div => div.remove())
 }
 
-grid(29, 29);
 
+function newGrid() {
+    button.addEventListener('click', () => {
+    reset();
+    user1 = prompt("How many rows do you want?");
+    user2 = prompt("How many columns do you want?");
+    if (user1 <= 100 && user2 <= 100) {
+        grid(user1, user2);
+    }
+    else {
+        alert('Number of rows and columns needs to be less than 100')
+        grid(16, 16);
+    }
+    
+})
+}
+newGrid();
 
